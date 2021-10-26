7f76b3bd4aa35500bc530141e9f898d07a95dd4b
Brenda-Machado <87547436+Brenda-Machado@users.noreply.github.com>
2021-10-26 13:57:36 -0300

    Update membro4.py

--- membro4.py
+++ membro4.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro4" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou a Brenda Machado. \nTenho 17 anos e comecei a aprender programação através de um curso Java da Alura. \nGosto de ler, desenhar e tocar trompete.\nEspero aprofundar meus conhecimentos em POO e Python." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
