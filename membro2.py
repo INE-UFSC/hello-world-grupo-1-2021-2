f459af5473dcd6be451c79478ffe656674e1eef7
gabi-FS <gabrielafdasilveira@gmail.com>
2021-10-27 12:05:29 -0300

    update membro4 - Gabriela

--- membro2.py
+++ membro2.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro2" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou a Gabriela Furtado da Silveira. \nTenho 18 anos, gosto de cantar, tocar teclado e fazer karatê. \nToda a minha experiência de programação foi no curso de CCO (bem pouca), então espero aprender bastante." #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
