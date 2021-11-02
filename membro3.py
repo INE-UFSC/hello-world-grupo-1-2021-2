10f3e16948ff90913ab883a0638069fdb2fb6f62
Lucas Cardozo <themegatroll009@gmail.com>
2021-11-02 17:39:02 -0300

    Update membro3.py

--- membro3.py
+++ membro3.py
@@ -1,2 +1,2 @@
 def hello():
-	return "Olá, eu sou o membro3" #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
+	return "Olá, eu sou o Lucas Cardozo \ntenho 18 anos,meus hobbies são jogar e assistir filmes \ntoda minha experiência com programação foi aqui no curso,então espero aprofundar ainda mais no que ja aprendi   " #escreva aqui a sua apresentação - nome, idade, hobbies, experiência prévia de programação, e o que você espera desta disciplina
